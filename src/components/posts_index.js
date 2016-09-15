0cf4eef10d8beb31bac7260e548634f3a47b9122
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-15 11:25:53 -0400

    fetch data in lifecycle method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return (
-    <div>List of blog posts</div>
-  );
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts:</div>
+    );
+  }
+}
+
+// could eliminate this function and simply pass in an object
+// to the connect funtion below, like this:
+// connect(null, { fetchPosts })(PostsIndex)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
