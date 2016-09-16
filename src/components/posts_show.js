78e1b7b7579837ec55f5cc84aad7153ccf355ea7
Stephen Mayeux <stephenmayeux@gmail.com>
2016-09-15 21:13:32 -0400

    delete button

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id);
   }
 
   render() {
@@ -16,6 +20,7 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/" className="btn btn-primary">Go Back</Link>
+        <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger pull-xs-right">Delete</button>
         <h3>{this.props.post.title}</h3>
         <p>{this.props.post.content}</p>
       </div>
@@ -27,4 +32,4 @@ function mapStateToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
